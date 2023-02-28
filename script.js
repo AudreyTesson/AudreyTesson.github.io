cb77976ae1a6fd629b881e1a0908bb4fa55caec0
Audrey Tesson <audrey.tesson.pro@outlook.fr>
2023-02-28 10:59:40 +0100

    modifs en cours

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// let list = document.querySelectorAll(".navigation li");
-// function activeLink() {
-//   list.forEach((item) => item.classList.remove("hovered"));
-//   this.classList.add("hovered");
-// }
-// list.forEach((item) => item.addEventListener("mouseover", activeLink));
-
-// let toggle = document.querySelector(".toggle");
-// let navigation = document.querySelector(".navigation");
-// let main = document.querySelector(".main");
-
-// toggle.onclick = function () {
-//   navigation.classList.toggle("active");
-//   main.classList.toggle("active");
-// };
-
-
-
-
 // TIMELINE 
 
 const allRonds = document.querySelectorAll('.rond');
@@ -46,53 +27,6 @@ allBoxes.forEach(workCard => {
 
 })
 
-// TEXTE CACHE 
-
-// const button = document.querySelector('button');
-// const hiddenText = document.querySelector('.hidden-text');
-// const workCard = document.querySelector('.workCard');
-
-// button.addEventListener('click', function() {
-//    workCard.style.height = '800px';
-//    workCard.style.width = '600px';
-// });
-
-
-// button.addEventListener('click', function() {
-//    if (hiddenText.style.display === 'none') {
-//       hiddenText.style.display = 'block';
-//       button.textContent = 'Réduire';
-//    } else {
-//       hiddenText.style.display = 'none';
-//       button.textContent = 'En savoir plus';
-//    }
-// });
-
-// const button = document.querySelector('button');
-// const hiddenText = document.querySelector('.hidden-text');
-// const workCard = document.querySelector('.workCard');
-
-// button.addEventListener('click', function() {
-//    if (hiddenText.style.display === 'none') {
-//     workCard.style.display = 'block';
-//       hiddenText.style.display = 'block';
-//       workCard.style.zIndex = '999';
-//       button.textContent = 'Réduire';
-//    } else {
-//       hiddenText.style.display = 'none';
-//       workCard.style.zIndex = '1';
-//       button.textContent = 'En savoir plus';
-//    }
-// });
-
-
-// document.addEventListener('click', function(event) {
-//   if (!workCard.contains(event.target) && workCard.style.display === 'block') {
-//      hiddenText.style.display = 'none';
-//      button.textContent = 'Cliquez ici pour afficher le conteneur';
-//   }
-// });
-
 // EN SAVOIR PLUS 
 
 const parentContainer =  document.querySelector('.box-timeline');
@@ -112,3 +46,4 @@ parentContainer.addEventListener('click', event=>{
     current.textContent = current.textContent.includes('En savoir plus') ? "Réduire" : "En savoir plus";
 
 })
+
