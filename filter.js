4e96b88716893dd2f29dcb92e030de92a5855846
Audrey Tesson <audrey.tesson.pro@outlook.fr>
2023-04-20 16:54:39 +0200

    modifications hardskills

--- filter.js
+++ filter.js
@@ -1,4 +1,3 @@
-
 const filterButtons = document.querySelectorAll('.filter-button');
 const projects = document.querySelectorAll('.project');
 
@@ -22,3 +21,30 @@ filterButtons.forEach(button => {
     });
   });
 });
+
+
+
+const filterSkillsButtons = document.querySelectorAll('.filter-skills-button');
+const hardskills = document.querySelectorAll('.skill');
+
+filterSkillsButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const filter = button.dataset.filter;
+
+    // Change active button
+    filterSkillsButtons.forEach(button => {
+      button.classList.remove('active');
+    });
+    button.classList.add('active');
+
+    // Filter projects
+    hardskills.forEach(project => {
+      if (filter === 'all') {
+        project.style.display = 'block';
+      } else {
+        project.classList.contains(filter) ? project.style.display = 'block' : project.style.display = 'none';
+      }
+    });
+  });
+});
+
