c235d66d33ff1891f01c11a6b18adf23a5d5350c
Audrey Tesson <audrey.tesson.pro@outlook.fr>
2023-04-22 21:59:46 +0200

    many tries

--- js/hiddentext.js
+++ js/hiddentext.js
@@ -20,8 +20,31 @@
 // }
 
 
+// const texte = document.querySelector('.text');
+// const bouton = document.querySelector('.button');
+// const longueurLimite = 200;
+
+// if (texte.textContent.length > longueurLimite) {
+//   const texteCourt = texte.textContent.substring(0, longueurLimite);
+//   const texteLong = texte.textContent;
+//   texte.textContent = texteCourt + '...';
+//   bouton.style.display = 'block';
+
+//   bouton.addEventListener('click', function() {
+//     if (bouton.textContent === 'En savoir plus') {
+//       texte.textContent = texteLong + ' ';
+//       bouton.textContent = 'Réduire';
+//     } else {
+//       texte.textContent = texteCourt + '...';
+//       bouton.textContent = 'En savoir plus';
+//     }
+//   });
+// }
+
+
 const texte = document.querySelector('.text');
 const bouton = document.querySelector('.button');
+const projet = document.querySelector('.project');
 const longueurLimite = 200;
 
 if (texte.textContent.length > longueurLimite) {
@@ -34,9 +57,11 @@ if (texte.textContent.length > longueurLimite) {
     if (bouton.textContent === 'En savoir plus') {
       texte.textContent = texteLong + ' ';
       bouton.textContent = 'Réduire';
+      projet.classList.add('expanded'); // Ajouter la classe "expanded"
     } else {
       texte.textContent = texteCourt + '...';
       bouton.textContent = 'En savoir plus';
+      projet.classList.remove('expanded'); // Supprimer la classe "expanded"
     }
   });
 }
